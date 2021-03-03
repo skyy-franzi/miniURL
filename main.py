3987c035fcc6161a3d40f7a7e1655d3d94c5133f
Franziska Halischafsky <franziska.halischafsky@closed.com>
2021-03-03 16:29:51 +0100

    Post

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__, static_folder="static/", static_url_path="/static", template_folder="templates/")
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html', title='Minimal Url')
+    if request.method == "POST":
+        get_url = request.form["miniurl"]
+        return get_url
+    else:
+        return render_template('index.html', title='Minimal Url')
+
